eba5643e6f7f76b92237ca30eafda351b1fffef6
Taeho <ice_98@naver.com>
2023-01-22 00:57:31 +0900

    01-21

--- hello-spirng/src/main/java/hello/hellospirng/SpringConfig.java
+++ hello-spirng/src/main/java/hello/hellospirng/SpringConfig.java
@@ -1,14 +1,25 @@
 package hello.hellospirng;
 
+import hello.hellospirng.repository.JdbcMemberRepository;
 import hello.hellospirng.repository.MemberRepository;
 import hello.hellospirng.repository.MemoryMemberRepository;
 import hello.hellospirng.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 //스프링이 뜰때 Configuration을 읽고 스프링빈에 memberService와 memberRepository를 등록해준다
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource; //추가해줘야함
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -18,6 +29,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();//인터페이스는 new가 안되므로 구현체인 MemoryMemberREpository를 사용
+//        return new MemoryMemberRepository();//인터페이스는 new가 안되므로 구현체인 MemoryMemberREpository를 사용
+        //이제는 H2 DB를 쓸거기떄문에 위에껀 주석처리
+        return new JdbcMemberRepository(dataSource);
     }
 }
